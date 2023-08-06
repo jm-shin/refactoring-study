916b1fdaa2506f2d6f868e2ef54852344cdf3047
JongMin Shin <ace4463@gmail.com>
2023-08-06 16:02:15 +0900

    6-1 출력 로직 메서드 추출

--- ch6/1.extract-function.js
+++ ch6/1.extract-function.js
@@ -1,9 +1,19 @@
-function printOwing(invoice) {
-    let outstanding = 0;
-
+function printBanner() {
     console.log("***********");
     console.log("**고객채무***");
     console.log("***********");
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function printOwing(invoice) {
+    let outstanding = 0;
+
+    printBanner();
 
     // 미해결 채무를 계산한다
     for (const o of invoice.orders) {
@@ -15,7 +25,5 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()  + 30);
 
     // 세부사항을 출력한다.
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    printDetails(invoice, outstanding);
 }
